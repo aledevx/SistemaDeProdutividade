63372109e1f0ca940463226e048deb38a5a6a1df
Alexandre Oliveira <ale.devx@gmail.com>
2024-05-01 17:30:51 -0400

    feat: configuração dos serviços de repositories na injeção de dependencia do projeto de infraestrutura

--- SistemaDeProdutividade.Infrastructure/DependencyInjectionExtension.cs
+++ SistemaDeProdutividade.Infrastructure/DependencyInjectionExtension.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SistemaDeProdutividade.Domain.Repositories.Usuario;
 using SistemaDeProdutividade.Infrastructure.DataAccess;
+using SistemaDeProdutividade.Infrastructure.DataAccess.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,7 @@ public static class DependencyInjectionExtension
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         AddDbContext(services, configuration);
+        AddRepositories(services);
     }
 
     private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
@@ -23,5 +26,10 @@ public static class DependencyInjectionExtension
         {
             options.UseSqlServer(conectionString, b => b.MigrationsAssembly("SistemaDeProdutividade.API"));
         });
+    }
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUsuarioWriteOnlyRepository, UsuarioRepository>();
+        services.AddScoped<IUsuarioReadOnlyRepository, UsuarioRepository>();
     }
 }
