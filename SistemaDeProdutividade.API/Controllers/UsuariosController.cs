446334f1d6d68637207c8527a2de89369480d7e5
Alexandre Oliveira <ale.devx@gmail.com>
2024-05-01 17:32:42 -0400

    feat: add controller de usuário e implementação do endpoint para cadastro de usuário

---
+++ SistemaDeProdutividade.API/Controllers/UsuariosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeProdutividade.Communication.Requests;
+using SistemaDeProdutividade.Communication.Responses;
+using SistemaDeProdutividade.Domain.Contracts;
+
+namespace SistemaDeProdutividade.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class UsuariosController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(MensagemSucessoCadastroResponseJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Cadastrar([FromServices] ICadastrarUsuarioUseCase useCase, [FromBody] CadastrarUsuarioRequestJson request) 
+    {
+        var result = await useCase.Execute(request);
+
+        return Created(string.Empty, result);
+    }
+    public IActionResult Editar()
+    {
+        return Ok();
+    }
+    public IActionResult Visualizar()
+    {
+        return Ok();
+    }
+}
