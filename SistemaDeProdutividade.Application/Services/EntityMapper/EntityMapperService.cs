89e392da172ac79b8b66d31058aa4102eaff6ee1
Alexandre Oliveira <ale.devx@gmail.com>
2024-05-01 17:23:32 -0400

    feat: implementação de serviço para mapear entidade usuario com os dados da request

---
+++ SistemaDeProdutividade.Application/Services/EntityMapper/EntityMapperService.cs
@@ -0,0 +1,19 @@
+﻿using SistemaDeProdutividade.Communication.Requests;
+using SistemaDeProdutividade.Domain.Contracts;
+using SistemaDeProdutividade.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeProdutividade.Application.Services.EntityMapper;
+public class EntityMapperService : IRequestEntityMapperService
+{
+    public Usuario MappingToUsuario(CadastrarUsuarioRequestJson request)
+    {
+        var usuario = new Usuario(request.Nome, request.Matricula, request.Cpf, request.Perfil);
+
+        return usuario;
+    }
+}
---
+++ SistemaDeProdutividade.Domain/Contracts/IRequestEntityMapperService.cs
@@ -0,0 +1,13 @@
+﻿using SistemaDeProdutividade.Communication.Requests;
+using SistemaDeProdutividade.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDeProdutividade.Domain.Contracts;
+public interface IRequestEntityMapperService
+{
+    Usuario MappingToUsuario(CadastrarUsuarioRequestJson request);
+}
