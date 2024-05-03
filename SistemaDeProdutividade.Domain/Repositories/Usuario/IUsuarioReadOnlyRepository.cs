cb9fe42f3bbd0727f024fcb3432d196ca7a4f6fe
Alexandre Oliveira <ale.devx@gmail.com>
2024-05-03 18:16:21 -0400

    feat: add novo método no contrato de IUsuarioReadOnlyRepository

--- SistemaDeProdutividade.Domain/Repositories/Usuario/IUsuarioReadOnlyRepository.cs
+++ SistemaDeProdutividade.Domain/Repositories/Usuario/IUsuarioReadOnlyRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace SistemaDeProdutividade.Domain.Repositories.Usuario;
+﻿using SistemaDeProdutividade.Communication.Responses;
+
+namespace SistemaDeProdutividade.Domain.Repositories.Usuario;
 public interface IUsuarioReadOnlyRepository
 {
     Task<bool> ExisteUsuarioCpf(string cpf);
     Task<bool> ExisteUsuarioMatricula(string matricula);
+    Task<UsuarioResponseJson> BuscarUsuarioCpf(string cpf);
 }
